f7b11e08c35e487fa7dd3a218268b35eeca40488
Andy Bluntish <andy@bluntish.net>
2015-05-08 19:38:56 +1000

    Merge passed in options with default options
    Set options in `included` hook
    
    Merge `defaultOptions` in to the passed in options
    
    Check for `options.enabled` in `postprocessTree`.

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ var replace = require('broccoli-replace');
 
 module.exports = {
   name: 'ember-cli-replace',
+
   included: function(app) {
-    this._super.included.apply(this,arguments);
-    this.options = app.options.replace;
-  },
-  postprocessTree: function (type, tree) {
-    var options = this.options || {
+    this._super.included.apply(this, arguments);
+    this.options = app.options.replace || {};
+
+    var defaultOptions = {
       files: [],
       patterns: [],
       enabled: true
     };
-    if (options !== false && type === 'all') {
-      tree = replace(tree, options);
+
+    for (var option in defaultOptions) {
+      if (!this.options.hasOwnProperty(option)) {
+        this.options[option] = defaultOptions[option];
+      }
+    }
+  },
+
+  postprocessTree: function(type, tree) {
+    if (type === 'all' && this.options && this.options.enabled) {
+      tree = replace(tree, this.options);
     }
 
     return tree;
